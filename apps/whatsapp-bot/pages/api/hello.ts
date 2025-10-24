5d071d91401ca67aecfe319f9c07ca14d4adfe7a
gothd <ruanalt@aol.com>
2025-10-24 13:45:23 -0300

    endpoint do bot em json + imagem publica

--- apps/whatsapp-bot/pages/api/hello.ts
+++ apps/whatsapp-bot/pages/api/hello.ts
@@ -1,21 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-  description: string
-  imageUrl: string
-}
+  name: string;
+  description: string;
+  imageUrl: string;
+};
 
-export default function handler (
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const isProd = process.env.NODE_ENV === "production";
+  const protocol = isProd ? "https" : "http"; // assume que você está usando https em produção
+  const host = req.headers.host;
+  const fullUrl = `${protocol}://${host}`;
+
   res.status(200).json({
-    name: 'Göst',
-    description: 'Eu sou um gatinho, e durmo na roupa do chefe.',
-    imageUrl: `${
-      process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000'
-    }/20251014.jpg`
-  })
+    name: "Göst",
+    description: "Eu sou um gatinho, e durmo na roupa do chefe.",
+    imageUrl: `${fullUrl}/20251014.jpg`,
+  });
 }
